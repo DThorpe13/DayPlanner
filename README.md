7004c0a278abe5c7ebc2cbae340b1ad774bb18dd
Derek Thorpe <derekthorpe13@gmail.com>
2020-09-27 17:17:28 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DayPlanner
+
+As an employee, you might find yourself overwhelmed during the day at times, this project was created to help with organization. 
+
+A day scheduler that will store any input in your local storage, alter colors according to past, present, and future data, and ultimately keep your day to day schedule right in front of you!
+
+You will find example pictures of the project in use inside of the assets folder.
--- javascript.js
+++ javascript.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function () {
         
         console.log(this);
-        var text = $(this).siblings(".description").val();
+        var input = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time, text);
+        localStorage.setItem(time, input);
     })
     
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
 
     function hourTracker() {
